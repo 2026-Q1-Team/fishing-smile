08592dc0f75fd2e88383eb734a7ab2f0e68093b1
C.Garunngampun <spacekelp46@gmail.com>
2026-01-08 15:43:17 +0700

    Updated to match working version.

--- src/tracking.php
+++ src/tracking.php
@@ -1,35 +1,30 @@
 <?php
-// track.php
-
 if (isset($_GET['k'])) {
     $servername = "localhost";
     $username   = "tracker";
     $password   = "fishtrack67";
-    $dbname     = "fishtrack"; 
-
+    $dbname     = "fishtrack";
     try {
         $conn = new mysqli($servername, $username, $password, $dbname);
-
         $stmt = $conn->prepare(
-            "SELECT track_key FROM fishlist WHERE fishlist.track_key = ?"
+            "SELECT track_key FROM fishlist WHERE track_key = ?"
         );
         $stmt->bind_param("s", $_GET['k']);
         $stmt->execute();
         $result = $stmt->get_result();
         $row = $result->fetch_assoc();
         $stmt->close();
-    
-        if ($row == $_GET['k']) {
+
+        if ( $row == $_GET['k']) {
             $stmt2 = $conn->prepare(
-                "INSERT INTO fishlog (track_key, datetime)
-                    VALUES (?, NOW())"
+                "INSERT INTO fishlog (track_key, datetime) VALUES (? , NOW())"
             );
-            $stmt2->bind_param("s", $_GET['k']);
+            $stmt2->bind_param("s", $GET['k']);
             $stmt2->execute();
             $stmt2->close();
-        
-            $conn->close();
         }
+        $conn->close();
+
         // Invisible success
         http_response_code(200);
     } catch (Exception $e) {
