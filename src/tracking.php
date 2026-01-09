056e589ce3fb8c9d33b13215f8005c8f3245b7bc
SiwawawaXD <149519770+SiwawawaXD@users.noreply.github.com>
2026-01-09 09:05:40 +0700

    copied from live version

--- src/tracking.php
+++ src/tracking.php
@@ -4,8 +4,9 @@ if (isset($_GET['k'])) {
     $username   = "tracker";
     $password   = "fishtracker67";
     $dbname     = "fishtrack";
+
     $conn = new mysqli($servername, $username, $password, $dbname);
-    $stmt = $conn->prepare("INSERT INTO fishlog (track_key, datetime) VALUES (? , NOW())");
+    $stmt = $conn->prepare( "INSERT INTO fishlog (track_key, datetime) VALUES (? , NOW())" );
     $stmt->bind_param("s", $_GET['k']);
     $stmt->execute();
     $stmt->close();
