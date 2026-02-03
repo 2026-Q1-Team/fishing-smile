7e31234b7b22ad59933ad5e59f10d937cd59f47d
C.Garunngampun <spacekelp46@gmail.com>
2026-02-03 14:25:28 +0700

    A lot changed.

--- src/cast_net.py
+++ src/cast_net.py
@@ -1,24 +1,17 @@
-import smtplib
-from email.mime.text import MIMEText
 import hashlib
+import numpy
 import pymysql
+import smtplib
+import datetime
+from email.mime.text import MIMEText
 from pymysql.err import MySQLError
 
 # Sender info (for SMTPlib)
 SENDER = "siwapon.so11@gmail.com"
 PASSWORD = "ajnr ulsc hxey tcnp"
 
-# Receiver matrix. Use ['email','name']. TODO add Thai name column, add read-from-file
-MList = [
-    ['siwapon.so11@gmail.com', 'Siwapon'],
-    ['siwaaltmail@gmail.com','Siwanon']
-]
 # Email subject.
 SUBJECT = "Please change your Organization account password."
-
-# Link to target website.
-FORM_LINK = "http://hii-survey.secteam.in.th/index.html?k="
-
 # Email body. TODO add Thai language version
 TEXT_MESSAGE = """Dear {name},
 According to our new security policy, all Organization staff must change their password every 6 months.
@@ -35,39 +28,58 @@ Please be reminded that all staff must change their password before 28 Jan 2026.
 Thank you for taking your time to keep our organization secure.
 - ICT Team.
 """
+# Link to target website.
+FORM_LINK = "http://hii-survey.secteam.in.th/index.html?k="
+
+
 # =====================================================
 
+def get_target(filename):
+    tm = numpy.genfromtxt(filename, delimiter=',', dtype=str)
+    return tm
+
+
 def insert2db(addr):
-    ct = hashlib.md5(addr.encode(), usedforsecurity=False)
+    ID = hashlib.md5(addr.encode(), usedforsecurity=False)
+    KEY = hashlib.md5(str(addr,key_batch).concat.encode(), usedforsecurity=False)
     try:
         con = pymysql.connect(user='tracker', password='fishtracker67', host='localhost', database='fishtrack')
         mycursor = con.cursor()
-        sql = "INSERT INTO fishlist (track_key, emailaddr) VALUES (%s, %s)"
-        value = (ct.hexdigest(), addr)
+        sql = "INSERT INTO fishlist (ID, EMAIL) VALUES (%s, %s)"
+        value = (ID.hexdigest(), addr)
         mycursor.execute(sql, value)
         con.commit()
+        sql2 = "INSERT INTO fishcast (ID, KEY, BATCH) VALUES (%s, %s, %s)"
+        value2 = (ID.hexdigest(), KEY.hexdigest(), key_batch)
+        mycursor.execute(sql2, value2)
         con.close()
     except pymysql.Error as e:
         print("could not close connection error pymysql %d: %s" % (e.args[0], e.args[1]))
-    return ct.hexdigest()
+    return KEY.hexdigest()
 
-def send_email(addr,name,key):
+
+def send_email(addr, name, key):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(SENDER, PASSWORD)
-    text_body = TEXT_MESSAGE.format(name=name, link=FORM_LINK+key)
-    msg = MIMEText(text_body, 'plain', 'utf-8')
+    text_body = TEXT_MESSAGE.format(name=name, link=FORM_LINK + key)
+    msg = MIMEText(text_body, 'html')
     msg['Subject'] = SUBJECT
     msg['From'] = SENDER
     msg['To'] = addr
     server.sendmail(SENDER, addr, msg.as_string())
     server.quit()
+
+
 # ======================================================
 
 if __name__ == "__main__":
     sent_count = 0
+    key_batch = datetime.datetime.now()
+    # Receiver matrix. Should be [['email','name_TH','name_EN']] when printed.
+    MList = get_target(input("Enter filepath:"))
     for e in MList:
         key = insert2db(e[0])
-        send_email(e[0],e[1],key)
+        send_email(e[0], e[1], key)
         sent_count += 1
-    print("Sent to ",sent_count," email(s).")
+    print("Sent to ", sent_count, " email(s).")
