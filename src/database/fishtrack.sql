6dff4156e36978bcefce40ef4b496501d97323b4
Apiwat Chantawibul <billiska@gmail.com>
2026-02-13 14:24:46 +0700

    Simplify auto increment and foreign key def
    Part of #3

--- src/database/fishtrack.sql
+++ src/database/fishtrack.sql
@@ -11,7 +11,7 @@ USE `fishtrack`;
 
 
 CREATE TABLE IF NOT EXISTS `Target Profile` (
-  `uid` int NOT NULL,
+  `uid` int NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL,
   `phone` varchar(16) NOT NULL,
@@ -22,77 +22,27 @@ CREATE TABLE IF NOT EXISTS `Target Profile` (
 
 
 CREATE TABLE IF NOT EXISTS `Attack` (
-  `uid` int NOT NULL,
+  `uid` int NOT NULL AUTO_INCREMENT,
   `UniqueRandomCode` varchar(32) DEFAULT NULL,
   `scheme` varchar(32) NOT NULL,
   `target` int NOT NULL,
-  PRIMARY KEY (`uid`)
+  PRIMARY KEY (`uid`),
+  FOREIGN KEY (`target`) REFERENCES `Target Profile` (`uid`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE IF NOT EXISTS `Event` (
-  `uid` int NOT NULL,
+  `uid` int NOT NULL AUTO_INCREMENT,
   `atk_id` int NOT NULL,
   `kind` varchar(64) DEFAULT NULL,
   `ts` datetime NOT NULL,
   `detail` json NOT NULL,
-  PRIMARY KEY (`uid`)
+  PRIMARY KEY (`uid`),
+  FOREIGN KEY (`atk_id`) REFERENCES `Attack` (`uid`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Attack`
---
-ALTER TABLE `Attack`
-  ADD KEY `fk_target1` (`target`);
-
---
--- Indexes for table `Event`
---
-ALTER TABLE `Event`
-  ADD KEY `fk_atk_id1` (`atk_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Attack`
---
-ALTER TABLE `Attack`
-  MODIFY `uid` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Event`
---
-ALTER TABLE `Event`
-  MODIFY `uid` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Target Profile`
---
-ALTER TABLE `Target Profile`
-  MODIFY `uid` int NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Attack`
---
-ALTER TABLE `Attack`
-  ADD CONSTRAINT `Attack_ibfk_1` FOREIGN KEY (`target`) REFERENCES `Target Profile` (`uid`) ON DELETE CASCADE;
-
---
--- Constraints for table `Event`
---
-ALTER TABLE `Event`
-  ADD CONSTRAINT `Event_ibfk_1` FOREIGN KEY (`atk_id`) REFERENCES `Attack` (`uid`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
