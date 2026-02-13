821e4ab9c46f70d828a3512769df77d0ed27e608
Apiwat Chantawibul <billiska@gmail.com>
2026-02-13 15:46:53 +0700

    define seach keys preemptively
    - describe informal "foreign keys" with python classes
      that are not in database.
    
    Part of #3

--- src/database/fishtrack.sql
+++ src/database/fishtrack.sql
@@ -17,31 +17,40 @@ CREATE TABLE IF NOT EXISTS `TargetProfile` (
   `phone` varchar(16) DEFAULT NULL,
   `company` varchar(64) DEFAULT NULL,
   `job_title` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY (`name`),
+  KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `Attack` (
   `id` int NOT NULL AUTO_INCREMENT,
   `external_id` char(32) NOT NULL,
+  -- Matches python AttackScheme.name value
   `scheme` varchar(32) NOT NULL,
   `target` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`external_id`),
   FOREIGN KEY (`target`) REFERENCES `TargetProfile` (`id`)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+  KEY (`scheme`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `Event` (
   `id` int NOT NULL AUTO_INCREMENT,
   `parent_attack` int DEFAULT NULL,
+  -- Matches `COMPONENT.KIND` pattern where
+  -- - `COMPONENT` matches python AttackComponet.kind value.
+  -- - `KIND` matches python describe event type associated with that AttackComponent.
   `kind` varchar(64) NOT NULL,
   `time` datetime NOT NULL,
   `detail` json DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`parent_attack`) REFERENCES `Attack` (`id`)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+  KEY (`kind`),
+  KEY (`time`, `kind`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
