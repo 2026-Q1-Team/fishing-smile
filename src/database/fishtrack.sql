35d4c8462e26eae11d090ad2894a05c0b13a3d13
Apiwat Chantawibul <billiska@gmail.com>
2026-02-13 15:06:39 +0700

    Adjust nullability
    - target profile maybe incomplete.
      - but we require name and email so it can be referenced with external
        sources
    - allow null parent attack, so we can record other kind of system event
      if needed to.
    - kind must not be null. We need it to decide how to understand what
      event it is and how to parse details.
    - details can be null. sometimes an event just does not have details.
    
    Part of #3

--- src/database/fishtrack.sql
+++ src/database/fishtrack.sql
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS `TargetProfile` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL,
-  `phone` varchar(16) NOT NULL,
-  `company` varchar(64) NOT NULL,
-  `job_title` varchar(64) NOT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `company` varchar(64) DEFAULT NULL,
+  `job_title` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -35,10 +35,10 @@ CREATE TABLE IF NOT EXISTS `Attack` (
 
 CREATE TABLE IF NOT EXISTS `Event` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `parent_attack` int NOT NULL,
-  `kind` varchar(64) DEFAULT NULL,
+  `parent_attack` int DEFAULT NULL,
+  `kind` varchar(64) NOT NULL,
   `time` datetime NOT NULL,
-  `detail` json NOT NULL,
+  `detail` json DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`parent_attack`) REFERENCES `Attack` (`id`)
     ON DELETE CASCADE
