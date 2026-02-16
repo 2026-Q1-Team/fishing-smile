c91d801789c40bd6afda31df23f95d17e83ded2e
Apiwat Chantawibul <billiska@gmail.com>
2026-02-16 16:51:44 +0700

    iterate over all test files
    For #12

--- src/fishing_smile/tests/core/model/test_attack_scheme.py
+++ src/fishing_smile/tests/core/model/test_attack_scheme.py
@@ -7,15 +7,12 @@ from fishing_smile.core.model import *
 
 
 @pytest.mark.parametrize(
-    'scheme_name',
-    [
-        'empty',
-        'change_password',
-    ]
+    'scheme_file',
+    list((Path(__file__).parent / 'test_cases/scheme').glob('*.yaml')),
 )
-def test_create_attack_scheme(scheme_name):
-    path = Path(__file__).parent / f'test_cases/scheme/{scheme_name}.yaml'
-    with open(path) as f:
+def test_create_attack_scheme(scheme_file):
+    scheme_name = scheme_file.stem
+    with open(scheme_file) as f:
         doc = yaml.safe_load(f)
     scheme = AttackScheme.model_validate(doc)
     assert scheme.name == scheme_name
