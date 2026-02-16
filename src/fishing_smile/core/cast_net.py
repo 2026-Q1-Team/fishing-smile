b89003bb594dc5e4ad66562b72ad9188b5e36851
c-foam <162972708+c-foam@users.noreply.github.com>
2026-02-16 13:48:53 +0700

    Updated to match new model.
    Signed-off-by: c-foam <162972708+c-foam@users.noreply.github.com>

--- src/fishing_smile/core/cast_net.py
+++ src/fishing_smile/core/cast_net.py
@@ -1,5 +1,6 @@
 import logging
 _logger = logging.getLogger(__name__)
+import secrets
 import datetime
 import hashlib
 import smtplib
@@ -11,10 +12,9 @@ from fishing_smile.settings import get_settings
 
 settings = get_settings()
 
-# Email subject.
+# TODO -- change to modular email templates
+URL = f"{settings.cast.url}/index.html?k="
 SUBJECT = "Please change your Organization account password."
-# Email body.
-#TODO -- add Thai language version
 TEXT_MESSAGE = """Dear {name_en},
 According to our new security policy, all Organization staff must change their password every 6 months.
 Follow these steps below to change your Organization account password.
@@ -30,49 +30,52 @@ Please be reminded that all staff must change their password before 28 Jan 2026.
 Thank you for taking your time to keep our organization secure.
 - ICT Team.
 """
-# Link to target website.
-URL = f"{settings.cast.url}/index.html?k="
 
 
-def insert2db(addr, key_batch):
-    # TODO -- change id/key-gen method
-    id = hashlib.md5(addr.encode(), usedforsecurity=False)
-    key = hashlib.md5(''.join([addr, str(key_batch)]).encode(), usedforsecurity=False)
-    # TODO -- change sql query to match new db
-    with pymysql.connect(settings().db.model_dump()) as connection:
+# TODO -- redesign the flow of cast_net
+# Entire flow should probably be changed to avoid repeatedly connecting and disconnecting to the sql server.
+
+
+def update_database(target, scheme):
+    urc = secrets.token_hex(16)  # token_urlsafe returns inconsistent string length
+    with pymysql.connect(**settings.db.model_dump()) as connection:
         with connection.cursor() as cursor:
-            sql = """
-                  INSERT INTO fishlist (`ID`, `EMAIL`)
-                  VALUES (%s, %s) \
-                  """
-            cursor.execute(id.hexdigest(), addr)
+            insert_profile = """
+            IF NOT EXISTS (SELECT * FROM `Target Profile` WHERE `email` = %(email))
+                INSERT INTO `Target Profile` (`name`, `email`, `phone`, `company`, `job_title`)
+                VALUES (%(name)s, %(email)s, %(phone)s, %(company)s, %(job_title)s)
+            ELSE
+                UPDATE `Target Profile`
+                SET (`name`, `phone`, `company`, `job_title`) = (%(name)s, %(phone)s, %(company)s, %(job_title)s)
+                WHERE `email` = %(email)s
+            """
+            insert_attack = """
+            INSERT INTO `Attack` (`external_id`, `scheme_name`, `target_id`)
+            VALUES (%(urc)s, %(scheme)s, %(target.uid)s)
+            """
+            cursor.execute(insert_profile, (target,))
+            cursor.execute(insert_attack,
+                           (urc, scheme, target.uid))  # Unsure about what the target part should be, fix later.
         connection.commit()
-    return key.hexdigest()
+    return urc
 
 
-def send_email(target, key):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(settings.cast.sender, settings.cast.password)
-    text_body = TEXT_MESSAGE.format(
-        **target._asdict(),
-        link=URL + key,
-    )
-    msg = MIMEText(text_body, 'html')
-    msg['Subject'] = SUBJECT
-    msg['From'] = settings.cast.sender
-    msg['To'] = target.email
-    server.sendmail(SENDER, target.email, msg.as_string())
-    server.quit()
+def send_email(target, urc):
+    with smtplib.SMTP("smtp.gmail.com", 587).starttls() as server:
+        with server.login(settings.cast.sender, settings.cast.password) as session:
+            msg = MIMEText(TEXT_MESSAGE.format(**target._asdict(), link=URL + urc, ), 'html')
+            msg['Subject'] = SUBJECT
+            msg['From'] = settings.cast.sender
+            msg['To'] = target.email
+            session.sendmail(settings.cast.sender, target.email, msg.as_string())
 
 
-def cast_net(targets):
+def cast_net(targets, scheme):
     sent_count = 0
-    key_batch = datetime.datetime.now()
     for target in targets:
-        k = insert2db(target.email, key_batch)
+        urc = update_database(target, scheme)
         try:
-            send_email(target, k)
+            send_email(target, urc)
             sent_count += 1
         except:
             print("Failed to send to", target.email)
