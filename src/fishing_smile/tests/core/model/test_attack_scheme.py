b63676c88876e760410e3734f42d2ec79c289803
Apiwat Chantawibul <billiska@gmail.com>
2026-02-16 17:01:46 +0700

    Split attack scheme test cases into valid/invalid
    Part of #12

--- src/fishing_smile/tests/core/model/test_attack_scheme.py
+++ src/fishing_smile/tests/core/model/test_attack_scheme.py
@@ -2,13 +2,17 @@ from pathlib import Path
 
 import pytest
 import yaml
+from pydantic import ValidationError
 
 from fishing_smile.core.model import *
 
 
+TEST_SCHEME_PATH = Path(__file__).parent / 'test_cases/scheme'
+
+
 @pytest.mark.parametrize(
     'scheme_file',
-    list((Path(__file__).parent / 'test_cases/scheme').glob('*.yaml')),
+    list(TEST_SCHEME_PATH.glob('valid/*.yaml')),
 )
 def test_create_attack_scheme(scheme_file):
     scheme_name = scheme_file.stem
@@ -16,3 +20,14 @@ def test_create_attack_scheme(scheme_file):
         doc = yaml.safe_load(f)
     scheme = AttackScheme.model_validate(doc)
     assert scheme.name == scheme_name
+
+
+@pytest.mark.parametrize(
+    'scheme_file',
+    list(TEST_SCHEME_PATH.glob('invalid/*.yaml')),
+)
+def test_invalid_attack_scheme(scheme_file):
+    with pytest.raises(ValidationError):
+        with open(scheme_file) as f:
+            doc = yaml.safe_load(f)
+        scheme = AttackScheme.model_validate(doc)
