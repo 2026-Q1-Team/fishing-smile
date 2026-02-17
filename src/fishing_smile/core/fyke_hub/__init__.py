ffca0d8498c3135b817f1a6f3d5e9eabf5bb9380
SiwawawaXD <149519770+SiwawawaXD@users.noreply.github.com>
2026-02-17 10:04:57 +0700

    Rewrite endpoints to use sqlmodel I currently use try except to make it response. Still don't knonw if there is a way to log error better than this which I will try to find. And the content of response json can be changed later, right now it just response backend data which I used to test endpoint. Signed-off-by: SiwawawaXD <149519770+SiwawawaXD@users.noreply.github.com>

--- src/fishing_smile/core/fyke_hub/__init__.py
+++ src/fishing_smile/core/fyke_hub/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import (
     BaseModel,
     Field,
@@ -6,7 +6,13 @@ from pydantic import (
 import pymysql
 
 from fishing_smile.settings import get_settings
-
+from sqlmodel import Field, Session, select
+from fishing_smile.database.engine import engine
+from fishing_smile.database.sqlmodel import SQLModel
+from fishing_smile.core.model import *
+from datetime import datetime
+import hashlib
+import json
 
 settings = get_settings()
 app = FastAPI(title = 'Fyke Hub: Handling interactions from anti-phish training participants')
@@ -17,24 +23,31 @@ app = FastAPI(title = 'Fyke Hub: Handling interactions from anti-phish training 
 @app.get('/change_password')
 async def change_password_ui(
     k: str,
+    request: Request
 ):
-    """Simulate fake "change password" HTML page.
+    client_host = request.client.host # This is used to get ip
+    try:
+        with Session(engine) as session:
+            result1 = session.exec(
+                select(AttackTable)
+                    .where(AttackTable.external_id == str(k))
+            ).first()
 
-    Also track the participant who visit this endpoint.
-    """
-    with pymysql.connect(**settings.db.model_dump()) as connection:
-        with connection.cursor() as cursor:
-            sql = """
-                INSERT INTO fishhook(`ID`, `KEY`, `CLICK`)
-                SELECT `ID`, `KEY`, NOW()
-                FROM fishcast
-                WHERE `KEY` = %s
-            """
-            # TODO: Log error when key does not match.
-            # Currently it just does not insert because select is empty.
-            cursor.execute(sql, (k,))
-        connection.commit()
+            event = EventTable(parent_attack_id=result1.id, kind="Email sent, Link clicked", time=datetime.now(), detail={'ip': client_host})
+            session.add(event)
+            session.commit()
+            session.refresh(event) 
+            return json.dumps({
+                "k" : k,
+                "result" : result1.id,
+                "event" : str(event.model_dump())
+            })
+    except Exception as e:
+        return f"error: {e}"
+        
     # TODO: shouldn't this also serve next-stage HTML payload?
+    # TODO: Log error when key does not match.
+    # Currently it just does not insert because select is empty.
 
 
 class ChangePasswordApiBody(BaseModel):
@@ -45,25 +58,32 @@ class ChangePasswordApiBody(BaseModel):
 @app.post('/api/change_password')
 async def change_password_api(
     body: ChangePasswordApiBody,
+    request: Request
 ):
-    """Simulate fake "change password" API.
+    client_host = request.client.host
+    try:
+        with Session(engine) as session:
+            result1 = session.exec(
+                select(AttackTable)
+                    .where(AttackTable.external_id == str(body.k))
+            ).first()
 
-    Also track the participant who visit this endpoint.
-    """
-    with pymysql.connect(**settings.db.model_dump()) as connection:
-        with connection.cursor() as cursor:
-            sql = """
-                INSERT INTO fishcook(`ID`, `KEY`, `PWND`, `TEXT`)
-                SELECT `ID`, `KEY`, NOW(), %s
-                FROM fishcast
-                WHERE `KEY` = %s
-            """
-            # TODO: should not be storing plain text password
-            # TODO: Log error when key does not match.
-            # Currently it just does not insert because select is empty.
-            cursor.execute(sql, (body.p, body.k))
-        connection.commit()
+            hashed_password = hashlib.sha256(body.p.encode('utf8')) # hash password
+
+            event = EventTable(parent_attack_id=result1.id, kind="Email sent, Link clicked, Password inserted", time=datetime.now(), detail={'ip': client_host, 'password' : hashed_password.hexdigest()})
+            session.add(event)
+            session.commit()
+            session.refresh(event) 
+            return json.dumps({
+                "k" : body.k,
+                "result" : result1.id,
+                "event" : str(event.model_dump())
+            })
+    except Exception as e:
+        return f"error: {e}"
+
     # This is the end of "change password" attack scheme.
     # TODO: Serve HTML explaining the red-flags of this attack scheme
     # to the user who fell through.
-
+    # TODO: Log error when key does not match.
+    # Currently it just does not insert because select is empty.
