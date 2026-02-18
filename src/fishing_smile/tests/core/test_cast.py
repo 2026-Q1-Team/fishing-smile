d0419af3f970eb4c61068e45c651facdfb7d32c0
Apiwat Chantawibul <billiska@gmail.com>
2026-02-19 01:20:31 +0700

    Simplify test condition
    Since sqlmodel already support relationship lookup in concise syntax,
    we should just use it as long as it's not the main subject being tested
    about.
    
    Part of #11

--- src/fishing_smile/tests/core/test_cast.py
+++ src/fishing_smile/tests/core/test_cast.py
@@ -15,18 +15,10 @@ def test_update_db(session):
         job_title = 'pro fisher',
     )
     ex_id_out = update_database(target, 'update_test', session)
-    result_profile = session.exec(
-        select(TargetProfileTable).where(
-            TargetProfileTable.name == target.name
-        )
-    ).all()
-    result_attack = session.exec(
-        select(AttackTable).where(
-            AttackTable.external_id == ex_id_out
-        )
-    ).all()
-    assert len(result_attack) == 1
-    assert result_profile[0].id == result_attack[0].target_id
+    result_attacks = session.exec(select(AttackTable)).all()
+    assert len(result_attacks) == 1
+    assert result_attacks[0].scheme_name == 'update_test'
+    assert result_attacks[0].target.email == 'address@domail.com'
 
 
 def test_send_mail():
