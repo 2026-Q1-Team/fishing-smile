afcc8e612527155492aca81fb0e689586af47905
Apiwat Chantawibul <billiska@gmail.com>
2026-02-20 15:19:12 +0700

    Wrap session fixture with outer transaction
    allowing session.commit used by test subjects to be rolled back
    after test ends.
    
    Resolves #13

--- src/fishing_smile/tests/conftest.py
+++ src/fishing_smile/tests/conftest.py
@@ -8,11 +8,21 @@ from fishing_smile.database.engine import engine
 from fishing_smile.core.model import *
 
 
+# NOTE: Adapted from https://gist.github.com/kissgyorgy/e2365f25a213de44b9a2
+# Depends on "obsecure" property of session begining inside an outer transaction
+# which allows `session.commit()` called by test subjects to be later rolled back
+# via the outer transaction.
+#
+# I said "obsecure" because I can't find explicit explanation of this behaviour
+# in offical doc.
 @pytest.fixture(name = 'session')
 def empty_database_session():
-    with Session(engine) as session:
+    connection = engine.connect()
+    transaction = connection.begin()
+    with Session(bind = connection) as session:
         session.exec(delete(EventTable))
         session.exec(delete(AttackTable))
         session.exec(delete(TargetProfileTable))
         yield session
-        session.rollback()
+    transaction.rollback()
+    connection.close()
