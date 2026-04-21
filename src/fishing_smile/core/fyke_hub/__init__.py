26453173a9260e4b6b7149564aeb3539737e83cb
c-foam <162972708+c-foam@users.noreply.github.com>
2026-04-21 09:56:59 +0700

    Added changes as proposed.

--- src/fishing_smile/core/fyke_hub/__init__.py
+++ src/fishing_smile/core/fyke_hub/__init__.py
@@ -1,25 +1,16 @@
-from datetime import datetime
 from pathlib import Path
-import hashlib
 
-from fastapi.openapi.models import Components
-from jinja2 import Template
+from argon2 import PasswordHasher
 from fastapi import FastAPI, Request, Depends, HTTPException
-from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import (
-    BaseModel,
-    Field,
-)
-from sqlmodel import Field, Session, select
-from argon2 import PasswordHasher
+from sqlmodel import Session, select
 
-from fishing_smile.settings import get_settings
-from fishing_smile.database.engine import get_session
-from fishing_smile.database.sqlmodel import SQLModel
 from fishing_smile.core.model import *
+from fishing_smile.database.engine import get_session
+from fishing_smile.settings import get_settings
 
 settings = get_settings()
 app = FastAPI(title = 'Fyke Hub: Handling interactions from anti-phish training participants')
@@ -35,83 +26,84 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def create_endpoint(scheme_name, component, method):
+    async def universal_endpoint(
+            request: Request,
+            k: str,
+            p: str | None = None,
+            session: Session = Depends(get_session),
+    ):
+        attack = session.exec(
+            select(AttackTable)
+            .where(AttackTable.external_id == k)
+        ).first()
+        if (
+                attack == None
+                or attack.scheme_name != scheme_name
+        ):
+            raise HTTPException(
+                status_code = 404,
+                # NOTE: Report error using the neutral term `session` instead of `attack`
+                # because at this point we are still trying to deceive target.
+                detail = 'Key does not match existing session',
+            )
+
+        event_detail = {}
+        if component.templates['eventdetail'].value == 'ip':
+            event_detail  = {'ip': request.client.host}
+            event = EventTable(
+                parent_attack_id = attack.id,
+                kind = component.templates['eventkind'].value,
+                detail = event_detail,
+            )
+            session.add(event)
+            session.commit()
+        elif component.templates['eventdetail'].value == 'ip, password':
+            hashed_password = PasswordHasher().hash(p)
+            event_detail  = {
+                'ip': request.client.host,
+                'password' : hashed_password,
+            }
+            event = EventTable(
+                parent_attack_id = attack.id,
+                kind = str(component.templates['eventkind'].value),
+                detail = event_detail,
+            )
+            session.add(event)
+            session.commit()
+
+
+        scheme = attack.scheme
+        if method == 'GET':
+            html_content = component.templates['html'].jinja.render(attack = attack)
+            return HTMLResponse(content = html_content)
+        elif method == 'POST':
+            all_red_flags = []
+            for comp in scheme.components:
+                all_red_flags.extend(comp.red_flags)
+
+            html_component = scheme.components.first(name ='form_page')
+            html_content = html_component.templates['html'].jinja.render(
+                scheme_name=scheme.name,
+                description=scheme.description or "",
+                red_flags=[
+                    {"name": rf.name, "explanation": rf.explanation}
+                    for rf in all_red_flags
+                ],
+            )
+            return HTMLResponse(content=html_content)
+    return universal_endpoint
+
 # Dynamic Endpoint Creation
 for scheme_name in standard_schemes.schemes:
     for component in standard_schemes.get(scheme_name).components:
-        if component.kind == 'web' and ( component.templates['method'].value == 'GET' or component.templates['method'].value == 'POST'):
-            def create_endpoint(scheme_name, component, method):
-                async def universal_endpoint(
-                    request: Request,
-                    k: str,
-                    p: str | None = None,
-                    session: Session = Depends(get_session),
-                ):
-                    attack = session.exec(
-                        select(AttackTable)
-                            .where(AttackTable.external_id == k)
-                    ).first()
-                    if (
-                        attack == None
-                        or attack.scheme_name != scheme_name
-                    ):
-                        raise HTTPException(
-                            status_code = 404,
-                            # NOTE: Report error using the neutral term `session` instead of `attack`
-                            # because at this point we are still trying to deceive target.
-                            detail = 'Key does not match existing session',
-                        )
-
-                    event_detail = {}
-                    if component.templates['eventdetail'].value == 'ip':
-                        event_detail  = {'ip': request.client.host} 
-                        event = EventTable(
-                            parent_attack_id = attack.id,
-                            kind = component.templates['eventkind'].value,
-                            detail = event_detail,
-                        )
-                        session.add(event)
-                        session.commit()
-                    elif component.templates['eventdetail'].value == 'ip, password':
-                        hashed_password = PasswordHasher().hash(p)
-                        event_detail  = {
-                            'ip': request.client.host,
-                            'password' : hashed_password,
-                        }
-                        event = EventTable(
-                        parent_attack_id = attack.id,
-                        kind = str(component.templates['eventkind'].value),
-                        detail = event_detail,
-                        )
-                        session.add(event)
-                        session.commit()
-                    
-
-                    scheme = attack.scheme
-                    if method == 'GET':
-                        html_content = component.templates['html'].jinja.render(attack = attack)
-                        return HTMLResponse(content = html_content)
-                    elif method == 'POST':
-                        all_red_flags = []
-                        for comp in scheme.components:
-                            all_red_flags.extend(comp.red_flags)
-
-                        html_component = scheme.components.first(name ='form_page')
-                        html_content = html_component.templates['html'].jinja.render(
-                            scheme_name=scheme.name,
-                            description=scheme.description or "",
-                            red_flags=[
-                                {"name": rf.name, "explanation": rf.explanation}
-                                for rf in all_red_flags
-                            ],
-                        )
-                        return HTMLResponse(content=html_content)
-                return universal_endpoint
-
+        if component.kind != 'web' and ( component.templates['method'].value == 'GET' or component.templates['method'].value == 'POST'):
+            exit()
+        else:
             app.add_api_route(
                 path=  component.templates['url'].value,
                 endpoint=create_endpoint(scheme_name=scheme_name, component=component, method=component.templates['method'].value),
                 methods=[component.templates['method'].value],
                 response_class=HTMLResponse,
             )
-
 
