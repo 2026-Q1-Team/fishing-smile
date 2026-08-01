bf8facdd50fe41b5dd86d9798b7fee18313b9256
Apiwat Chantawibul <billiska@gmail.com>
2026-08-01 17:48:01 +0700

    Remove obsolete comment
    The database session fixture is recommended by
    https://docs.sqlalchemy.org/en/21/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites

--- src/fishing_smile/tests/conftest.py
+++ src/fishing_smile/tests/conftest.py
@@ -8,13 +8,6 @@ from fishing_smile.database.engine import engine
 from fishing_smile.core.model import *
 
 
-# NOTE: Adapted from https://gist.github.com/kissgyorgy/e2365f25a213de44b9a2
-# Depends on "obsecure" property of session begining inside an outer transaction
-# which allows `session.commit()` called by test subjects to be later rolled back
-# via the outer transaction.
-#
-# I said "obsecure" because I can't find explicit explanation of this behaviour
-# in offical doc.
 @pytest.fixture(name = 'session')
 def empty_database_session():
     connection = engine.connect()
